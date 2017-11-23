921b4a0e19036cbb8dab530289f73549ede032a4
hanif <hanif@appscode.com>
2017-11-23 15:47:22 +0600

    Update

--- hello_world.go
+++ hello_world.go
@@ -2,10 +2,12 @@ package test_go2
 
 import (
 	"fmt"
+	a "github.com/a8uhnf/test-go3"
 )
 
 func HelloWorld()  {
 	fmt.Println("Hello World from test-go2")
+	a.HelloWorld()
 }
 
 func HelloWorldTestDep()  {
