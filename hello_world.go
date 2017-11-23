3c52d4a1fd3624a99e5edb96a2318c2a4b666bb3
hanif <hanif@appscode.com>
2017-11-23 16:05:14 +0600

    Update

--- hello_world.go
+++ hello_world.go
@@ -2,15 +2,16 @@ package test_go2
 
 import (
 	"fmt"
-	a "github.com/a8uhnf/test-go3"
+	// a "github.com/a8uhnf/test-go3"
+	// "golang.org/x/tools/go/gcimporter15/testdata"
 )
 
 func HelloWorld()  {
 	fmt.Println("Hello World from test-go2")
-	a.HelloWorld()
+	// a.HelloWorld()
 }
 
 func HelloWorldTestDep()  {
 	fmt.Println("Hello World from test-go2. Branch: test-dep")
-	a.HelloWorld()
+	// a.HelloWorld()
 }
